de96b33b1fbb7f57910c951b645019d5732f40ab
e.kungurtsev <e.kungurtsev@2gis.ru>
2022-09-26 00:15:04 +0700

    Initial Commit

---
+++ SC Homework 5 part 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SC Homework 5 part 3
+//
+//  Created by Кунгурцев Эдуард Сергеевич on 26.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
